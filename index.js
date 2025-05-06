2c4285b3e8a7940fcdf153374ff87abd07101710
Mohottige.S.D. <146794439+Sahani-Mohottige@users.noreply.github.com>
2025-05-06 18:45:49 +0530

    Get and Post methods

--- index.js
+++ index.js
@@ -1,17 +1,90 @@
+const joi = require('joi');
 const express = require('express');
+const Joi = require('joi');
 const app = express();
+
+app.use(express.json());
+//middleware
 
 app.get('/',(req,res)=>{
     res.send("hello world!!");
 });
 
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+    {id: 4, name: 'course4'}
+]
+
 app.get('/api/courses',(req,res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id',(req,res)=>{
+    const course = courses.find(c =>c.id === parseInt(req.params.id));
+if(!course) res.status(404).send("Course not found");
+res.send(course);    
+});
+
+app.post('/api/courses',(req,res)=>{
+
+    const schema = Joi.object({
+      name: Joi.string().min(3).required()
+    });
+    
+    const result = schema.validate(req.body);
+
+    if(result.error){
+      res.status(400).send(result.error.details[0].message);
+      return;
+    }
+/*
+      { }
+
+  "name" is required
+
+    {
+      "name":"1"
+    }
+
+    "name" length must be at least 3 characters long
+*/
+
+    /*
+    if(!req.body.name || req.body.name.length <3){
+      res.status(400).send("Name is required and should have minimum 3 characters.");
+      return;
+    } 
+      */
+
+    const course = {
+       id:  courses.length+1,
+       name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+});
+    
+
+
+
+
+
+
+
+
+app.get('/api/classes',(req,res) => {
+    res.send([1,2,3]);
+});
+
+app.get('/api/classes/:id',(req,res)=>{
 res.send(req.params.id);
 });
+
+
+
+
 
 app.get('/api/posts/:year/:month',(req,res)=>{
     res.send(req.params);
