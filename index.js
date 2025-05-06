db76141f9531bbf16a264b7f53272d023ccc5e88
Mohottige.S.D. <146794439+Sahani-Mohottige@users.noreply.github.com>
2025-05-06 11:09:18 +0530

    practising end points

--- index.js
+++ index.js
@@ -2,11 +2,45 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("hello world");
+    res.send("hello world!!");
 });
 
 app.get('/api/courses',(req,res) => {
     res.send([1,2,3]);
 });
 
-app.listen(3000, () =>  console.log("Listening on port 3000..."));
+app.get('/api/courses/:id',(req,res)=>{
+res.send(req.params.id);
+});
+
+app.get('/api/posts/:year/:month',(req,res)=>{
+    res.send(req.params);
+});
+/*
+http://localhost:5000/api/posts/2025/5
+ 
+  {
+    "year": "2025",
+    "month": "5"
+  }
+*/
+
+app.get('/api/posts1/:year/:month',(req,res)=>{
+    res.send(req.query);
+});
+
+/*
+http://localhost:5000/api/posts1/2025/5?sortBy=name
+
+stored as key value pairs
+  {
+    "sortBy": "name"
+  }
+
+*/
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>  console.log(`Listening on port ${port}...`));
+//PS D:\Backend-with-Express> $env:PORT = 5000
+//Then - Listening on port 5000...
